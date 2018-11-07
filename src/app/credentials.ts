85cfeed4d4ac7813d67a815eec2a3a94ac36acfc
dkobrenski <dave@bbmdesigns.com>
2018-11-07 12:20:55 -0500

    creds for 0.0.2 needed?

---
+++ src/app/credentials.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDQB59kzXkcL_iwkLd7LaztITjNM0Q9cA0",
+    authDomain: "deep-time-app.firebaseapp.com",
+    databaseURL: "https://deep-time-app.firebaseio.com",
+    projectId: "deep-time-app",
+    storageBucket: "deep-time-app.appspot.com",
+    messagingSenderId: "583301525433"
+};
